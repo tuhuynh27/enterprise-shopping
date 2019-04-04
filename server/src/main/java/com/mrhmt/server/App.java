caa609ba6b53a848eeafdc9799bac41e2ea5acce
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-04-05 00:39:51 +0700

    remove unused parts

--- server/src/main/java/com/mrhmt/server/App.java
+++ server/src/main/java/com/mrhmt/server/App.java
@@ -5,11 +5,7 @@ package com.mrhmt.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories
