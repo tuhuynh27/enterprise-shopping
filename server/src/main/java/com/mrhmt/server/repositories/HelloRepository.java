9110a0b459e3ca2a20d854e2e509609bab7d0dfe
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-04-04 23:35:59 +0700

    remove hello

--- server/src/main/java/com/mrhmt/server/repositories/HelloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mrhmt.server.repositories;
-
-import com.mrhmt.server.entities.Hello;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloRepository extends JpaRepository<Hello, Long> {
-}
