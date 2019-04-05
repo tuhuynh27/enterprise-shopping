9884eabe6e70b4a600f00f8177411f92c1772ea3
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-04-05 17:44:03 +0700

    update home

--- webapp/src/app/home/home.component.ts
+++ webapp/src/app/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FakeService } from "../fake.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  photos: Array<any> = [];
 
-  constructor() { }
+  constructor(private fakeService: FakeService) {}
 
   ngOnInit() {
+    this.fakeService.getPhotos().subscribe(data => {
+      this.photos = data;
+    });
   }
-
 }
